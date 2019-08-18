ea89581a8942a47a6a8e0c6251c148818ab11432
Tyler Phillips <phillity@umail.iu.edu>
2019-08-18 17:13:38 -0400

    AES encryption/decryption and MD5 hashing

--- atallah.py
+++ atallah.py
@@ -1,38 +1,87 @@
 import hashlib
-from Crypto.Cipher import AES
 import binascii
+from Crypto.Cipher import AES
+
+"""
+'Dynamic and Efficient Key Management for Access Hierarchies'
+M. Atallah, et. al.
+https://dl.acm.org/citation.cfm?id=1455531
+"""
 
 
-def F(val_1, val_2, val_opt=None):
+def hash_fun(val_1, val_2, val_opt=None):
     """
-    Concats values and hashes using md5.
+    Concats values and hashes using md5. \\
+    hash = hash_fun(val_1||val_opt||val_2)
 
     Args:
-        val_1 (string): The first value to hash
-        val_2 (string): Second value to hash
-        val_opt (string): None by default, but should be able to pass in either "0" or "1" to be concated between values
+        val_1 (string): 16 byte hex value to hash.
+        val_2 (string): 16 byte hex value to hash.
+        val_opt (string): None by default, but should be able to pass in either
+                          "0" or "1" to be concated between values.
 
     Returns:
-        hex digest (string): the resulting hash
+        hex digest (string): the resulting hash.
     """
     message = ""
-
     if val_opt:
         message = val_1 + val_opt + val_2
     else:
         message = val_1 + val_2
 
-    return hashlib.md5(message.encode('utf-8')).hexdigest()
+    return hashlib.md5(message.encode("utf-8")).hexdigest()
 
 
-def Enc(r_ij, t_j, k_j):
-    y_ij = int(r_ij + r_ij, 16) ^ int(t_j + k_j, 16)
-    return format(y_ij, 'x').zfill(64)
+def encrypt(r_ij, t_j, k_j, nonce=bytes([42])):
+    """
+    AES encryption of t_j||k_j using r_ij key and nonce. \\
+    y_ij = AES.encrypt{r_ij}(t_j||k_j)
+
+    Args:
+        r_ij (string): 16 byte hex value to use as AES key.
+        t_j (string): 16 byte t_j value.
+        k_j (string): 16 byte k_j value.
+        nonce (bytes): bytes([42]) by default, byte array nonce for AES.
+
+    Returns:
+        y_ij (string): 32 byte ecryption.
+    """
+    aes = AES.new(bytes.fromhex(r_ij), AES.MODE_EAX, nonce=nonce)
+    y_ij, _ = aes.encrypt_and_digest(bytes.fromhex(t_j + k_j))
+
+    return y_ij.hex()
 
 
-def Dec(r_ij, y_ij):
-    t_j_and_k_j = int(r_ij + r_ij, 16) ^ int(y_ij, 16)
-    t_j_and_k_j = format(t_j_and_k_j, 'x').zfill(64)
+def decrypt(r_ij, y_ij, nonce=bytes([42])):
+    """
+    AES decryption of y_ij using r_ij key and nonce. \\
+    t_j||k_j = AES.decrypt{r_ij}(r_ij)
+
+    Args:
+        r_ij (string): 16 byte hex value to use as AES key.
+        y_ij (string): 32 byte ecryption of t_j||k_j using r_ij key and nonce.
+        nonce (bytes): bytes([42]) by default, byte array nonce for AES.
+
+    Returns:
+        t_j (string): 16 byte t_j value.
+        k_j (string): 16 byte k_j value.
+    """
+    aes = AES.new(bytes.fromhex(r_ij), AES.MODE_EAX, nonce=nonce)
+    t_j_and_k_j = aes.decrypt(bytes.fromhex(y_ij)).hex()
+
     t_j = t_j_and_k_j[:32]
     k_j = t_j_and_k_j[32:]
+
     return t_j, k_j
+
+
+"""
+def set(security_param, graph):
+
+    return pub, sec
+
+
+def derive(graph, pub, v_src, v_dst, s_src):
+
+    return found, k_dst
+"""
--- main.py
+++ main.py
@@ -1,47 +1,40 @@
 import numpy as np
 import pandas as pd
-from atallah import F, Enc, Dec
+from atallah import hash_fun, encrypt, decrypt
 import hashlib
 import os
 
-'''
-def set(security_param, graph):
-
-    return pub, sec
-
-
-def derive(graph, pub, v_src, v_dst, s_src):
-
-    return found, k_dst
-'''
-
 
 if __name__ == "__main__":
-    adjaceny_matrix = np.array([[1, 1, 1, 0], [0, 1, 0, 1], [0, 0, 1, 1], [0, 0, 0, 1]])
+    adjaceny_matrix = np.array([[1, 1, 1, 0],
+                                [0, 1, 0, 1],
+                                [0, 0, 1, 1],
+                                [0, 0, 0, 1]])
     df = pd.read_csv("breast-cancer.data")
     print("Objects: " + str(df.columns.values))
 
     # node i data
     s_i = hashlib.md5(os.urandom(16)).hexdigest()
     l_i = hashlib.md5(os.urandom(16)).hexdigest()
-    t_i = F(s_i, l_i, val_opt="0")
-    k_i = F(s_i, l_i, val_opt="1")
+    t_i = hash_fun(s_i, l_i, val_opt="0")
+    k_i = hash_fun(s_i, l_i, val_opt="1")
 
     # node j data
     s_j = hashlib.md5(os.urandom(16)).hexdigest()
     l_j = hashlib.md5(os.urandom(16)).hexdigest()
-    t_j = F(s_j, l_j, val_opt="0")
-    k_j = F(s_j, l_j, val_opt="1")
+    t_j = hash_fun(s_j, l_j, val_opt="0")
+    k_j = hash_fun(s_j, l_j, val_opt="1")
 
     # edge (i, j) data
-    r_ij = F(t_i, l_j)
-    y_ij = Enc(r_ij, t_j, k_j)
+    r_ij = hash_fun(t_i, l_j)
+    y_ij = encrypt(r_ij, t_j, k_j)
 
     # check it worked
-    t_i = F(s_i, l_i, val_opt="0")
-    r_ij = F(t_i, l_j)
+    t_i = hash_fun(s_i, l_i, val_opt="0")
+    r_ij = hash_fun(t_i, l_j)
     print(t_j)
     print(k_j)
-    t_j, k_j = Dec(r_ij, y_ij)
+
+    t_j, k_j = decrypt(r_ij, y_ij)
     print(t_j)
     print(k_j)
