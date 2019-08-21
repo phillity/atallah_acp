eea6d212fae6e47e1d6e96c290f2e716b76c0c62
haakensonb <untamedhawk44@gmail.com>
2019-08-21 15:54:16 -0400

    Added decorator function for timer

---
+++ decorators.py
@@ -0,0 +1,25 @@
+import functools
+import time
+
+def timer(function):
+    """
+    Decorator used to print the runtime of a function in seconds.
+    To use simply place "@timer" above a function declaration.
+    For nanoseconds instead use perf_counter_ns for start and end times.
+
+    Args:
+        function: the name of the function to time
+    
+    Returns:
+        The result of the function the decorator is called on
+    """
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        function_value = function(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"{function.__name__} ran in {run_time} seconds")
+        return function_value
+    return wrapper
